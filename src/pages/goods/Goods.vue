7b884c9201f392b4e9ba7073c4d56a16a53601da
liuyiliang07 <851260830@qq.com>
2020-08-28 11:21:35 +0800

    修改goods的样式

--- src/pages/goods/Goods.vue
+++ src/pages/goods/Goods.vue
@@ -22,13 +22,13 @@
           </el-form>
         </template>
       </el-table-column>
-      <el-table-column label="商品 ID" width="200" prop="_id"></el-table-column>
-      <el-table-column label="书籍名称" width="200" prop="bookName"></el-table-column>
-      <el-table-column label="作者" width="150" prop="author"></el-table-column>
-      <el-table-column label="价格" width="100" prop="line_price"></el-table-column>
-      <el-table-column label="库存" width="100" prop="num"></el-table-column>
-      <el-table-column label="描述" width="380" prop="desc"></el-table-column>
-      <el-table-column fixed="right" width="160">
+      <el-table-column label="商品 ID" width="200" prop="_id" :resizable='false'></el-table-column>
+      <el-table-column label="书籍名称" width="200" prop="bookName" show-overflow-tooltip :resizable='false'></el-table-column>
+      <el-table-column label="作者" width="150" prop="author" show-overflow-tooltip :resizable='false'></el-table-column>
+      <el-table-column label="价格" width="100" prop="line_price" :resizable='false'></el-table-column>
+      <el-table-column label="库存" width="100" prop="num" :resizable='false'></el-table-column>
+      <el-table-column label="描述" width="380" prop="desc" :resizable='false'></el-table-column>
+      <el-table-column fixed="right" width="160" :resizable='false'>
         <template slot="header" slot-scope="scope">
           <el-input
             v-model="searchText"
