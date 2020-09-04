71a4c282200bf392acfb603d89a2a85bd77a801d
marlen <1191765289@qq.com>
2020-09-04 21:57:37 +0800

    分类完成

---
+++ app/src/components/classifyContext.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="classifyContext">
+    <div class="small_class" :key="item.length" v-for="item in msg.list">
+      <h4 style="text-align:start">{{ item.name }}</h4>
+      <div class="dl_box">
+        <dl
+          class="_dl"
+          :key="child.name"
+          v-for="child in item.children[0].list"
+          @click="childselect(child.name)"
+        >
+          <dt>
+            <img :src="child.post" alt />
+          </dt>
+          <dd>{{child.name}}</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "classifyContext",
+  props: ["msg"],
+  data() {
+    return {};
+  },
+  methods: {
+    initclasscontext() {
+      console.log(this.msg);
+    },
+    childselect(table) {
+      console.log(table);
+      this.$emit("select", table);
+    },
+  },
+  created() {
+    this.initclasscontext();
+  },
+};
+</script>
+<style lang="scss">
+dl,
+dd,
+ul,
+li,
+div,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+main {
+  padding: 0;
+  margin: 0;
+}
+.small_class {
+  min-height: 100px;
+  padding-top: 14px;
+  padding-bottom: 24px;
+  padding-left: 20px;
+  h4 {
+    padding-bottom: 8px;
+  }
+  .dl_box {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    ._dl {
+      width: 80px;
+      height: 100px;
+      display: flex;
+
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      dt {
+        width: 60%;
+        padding-bottom: 5px;
+        img {
+          width: 100%;
+        }
+      }
+      dd {
+        width: 80px;
+      }
+    }
+  }
+}
+</style>
--- app/src/views/category/Category.vue
+++ app/src/views/category/Category.vue
@@ -1,5 +1,108 @@
 <template>
   <div class="category">
-    <h1>This is an category page</h1>
+    <van-search v-model="searchvalue" shape="round" background="#00abaf" placeholder="请输入搜索关键词" />
+    <van-tree-select
+      class="_v-container nav-bar"
+      :items="items"
+      height="94%"
+      :key="item.name"
+      v-for="(item, index) in classifymsg"
+      @click-nav="changeindex(index)"
+      :main-active-index.sync="active"
+    >
+      <template #content>
+        <div v-if="active === idx">
+          <div :key="item.length" v-for="item in item[idx].children">
+            <!-- {{ item }} -->
+            <classify-context :msg="item" @select="selectitem"></classify-context>
+          </div>
+        </div>
+      </template>
+    </van-tree-select>
   </div>
 </template>
+
+<script>
+import classifyContext from "../../components/classifyContext";
+
+export default {
+  components: {
+    classifyContext,
+  },
+  data() {
+    return {
+      active: 0,
+      idx: 0,
+      searchvalue: "",
+      classifymsg: [],
+      items: [],
+    };
+  },
+  created() {
+    this.initdata();
+  },
+  methods: {
+    onSearch() {
+      console.log("我是搜索框");
+    },
+    changeindex(idx) {
+      console.log(idx);
+      this.idx = this.active;
+      console.log(this.active);
+    },
+    // 这个是用于商品跳转的
+    selectitem(msg) {
+      console.log(msg);
+      console.log(this.$route);
+      console.log(this.$router);
+      this.$router.push("/goodslist/" + "tatle=" + msg);
+      console.log(this.$route);
+    },
+    async initdata() {
+      let data = await fetch(
+        "https://api.szbookmall.com/app/product_category/list",
+        {
+          method: "post",
+          headers: {
+            "Content-type": "application:/x-www-form-urlencoded:charset=UTF-8",
+          },
+        }
+      ).then((res) => res.json());
+      this.classifymsg = data.data;
+      console.log(data.data.list);
+      let classify = data.data.list;
+      this.items = classify.reduce((pre, item) => {
+        pre.push({ text: item.name });
+        return pre;
+      }, []);
+      console.log(this.items);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.category {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+  .nav-bar {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    .van-tree-select__nav-item {
+      flex: 1;
+    }
+
+    .van-tree-select__content {
+      flex: 3;
+    }
+    .van-sidebar-item--select::before {
+      width: 2px;
+      height: 30px;
+      background-color: #00abaf;
+    }
+  }
+}
+</style>
