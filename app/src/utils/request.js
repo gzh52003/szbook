69be8cdd3a5cf8e9982b1c79ae92c4db14f2b5c4
liuyiliang07 <851260830@qq.com>
2020-09-15 09:29:50 +0800

    提交一些小细节

--- app/src/utils/request.js
+++ app/src/utils/request.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const source = axios.CancelToken.source();
 
 const request = axios.create({
-  baseURL:"http://42.194.179.50/api",
+  baseURL:"http://www.ihuanu.cn/api",
   timeout:8*1000,
   withCredentials:false,
   headers:{'Content-Type': 'application/json;charset=UTF-8'},
